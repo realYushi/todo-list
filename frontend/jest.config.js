bce50efadc2e131436444bda7515fb6abed83b11
Yushi Cui <realyushi@gmail.com>
2024-06-24 23:19:20 +1200

    ✅ (frontend-ci.yml): fix npm test scripts to run correctly by updating the command names 🔧 (jest.config.js): update testMatch patterns to match JavaScript files under unit-tests folder ♻️ (jest.config.js): refactor moduleFileExtensions to include additional file extensions for Jest to recognize 📝 (package.json): update test-jest script to run Jest with experimental VM modules for compatibility

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,8 +1,10 @@
 export default {
     testEnvironment: "node",
     testMatch: [
-        "./tests/unit-tests/**/*.js?(x)",
-        "./tests/feature-tests/**/*.js?(x)",
-        "**/?(*.)+(spec|test).ts?(x)",
+        "**/tests/unit-tests/**/*.js", // Matches any JavaScript files under unit-tests
+        // "**/tests/feature-tests/**/*.js", // Matches any JavaScript files under feature-tests
+        // "**/?(*.)+(spec|test).[tj]s?(x)", // Matches all other test/spec files across the project
     ],
+    moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+    // Additional configurations like setupFiles, coverage options, etc., can go here
 };
