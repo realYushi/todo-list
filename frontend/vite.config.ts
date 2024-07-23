f85a9c653c3f47ebec82d145365815dc9fa6fdfd
Yushi Cui <realyushi@gmail.com>
2024-07-23 22:08:03 +1200

    🔧 (vite.config.ts): comment out mkcert plugin to disable HTTPS certificate generation for local development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
 import mkcert from "vite-plugin-mkcert"
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), mkcert()],
+  // plugins: [react(), tsconfigPaths(), mkcert()],
 
+  plugins: [react(), tsconfigPaths()],
   server: {
     open: true,
   },
